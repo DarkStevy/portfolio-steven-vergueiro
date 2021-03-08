af242d84284f5e7336d885f2e10089a08918f127
DarkStevy <vergueiro.steven@gmail.com>
2021-03-08 10:35:48 +0100

    master

--- src/components/main/Networks.jsx
+++ src/components/main/Networks.jsx
@@ -6,14 +6,10 @@ export default function Networks() {
     return (
         <div>
             <div>
-                <a target="_blank" href="https://www.linkedin.com/in/steven-vergueiro-3811671b2/">
-                    <LogosNetworks nameImage="linkedin"/>
-                </a>
+                <LogosNetworks nameImage="linkedin"/>
             </div>
             <div>
-                <a target="_blank" href="https://github.com/DarkStevy">
-                    <LogosNetworks nameImage="github"/>
-                </a>
+                <LogosNetworks nameImage="github"/>
             </div>
         </div>
     )
