72df8af5460fec0f5337171470c313477d0a03e4
DarkStevy <vergueiro.steven@gmail.com>
2021-03-08 10:15:54 +0100

    master

--- src/components/main/Networks.jsx
+++ src/components/main/Networks.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import LogosNetworks from '../images/logosNetworks';
 
 export default function Networks() {
 
@@ -7,12 +6,10 @@ export default function Networks() {
         <div>
             <div>
                 <a target="_blank" href="https://www.linkedin.com/in/steven-vergueiro-3811671b2/">
-                    <LogosNetworks nameImage="linkedin" />
                 </a>
             </div>
             <div>
                 <a target="_blank" href="https://github.com/DarkStevy">
-                    <LogosNetworks nameImage="github" />
                 </a>
             </div>
         </div>
